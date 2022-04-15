ab4b6db9ceae33bb96a54a65be6d84e257055187
Konrad Jazdzyk <konradsmkkd@gmail.com>
2022-04-15 23:03:52 +0200

    Genre, sort dropdown, tracks grid

---
+++ MobileApp/Mobile/Mobile/Mobile.Android/DropdownRenderer.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel;
+using Android.Content;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Mobile;
+using AndroidX.AppCompat.Widget;
+using Mobile.Droid;
+
+[assembly: ExportRenderer(typeof(Dropdown), typeof(DropdownRenderer))]
+namespace Mobile.Droid
+{
+    public class DropdownRenderer : ViewRenderer<Dropdown, AppCompatSpinner>
+    {
+        AppCompatSpinner spinner;
+        public DropdownRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Dropdown> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                spinner = new AppCompatSpinner(Context);
+                SetNativeControl(spinner);
+            }
+
+            if (e.OldElement != null)
+            {
+                Control.ItemSelected -= OnItemSelected;
+            }
+            if (e.NewElement != null)
+            {
+                var view = e.NewElement;
+
+                ArrayAdapter adapter = new ArrayAdapter(Context, Android.Resource.Layout.SimpleListItem1, view.ItemsSource);
+                Control.Adapter = adapter;
+
+                if (view.SelectedIndex != -1)
+                {
+                    Control.SetSelection(view.SelectedIndex);
+                }
+
+                Control.ItemSelected += OnItemSelected;
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var view = Element;
+            Control.DropDownVerticalOffset = 55;
+            if (e.PropertyName == Dropdown.ItemsSourceProperty.PropertyName)
+            {
+                ArrayAdapter adapter = new ArrayAdapter(Context, Android.Resource.Layout.SimpleListItem1, view.ItemsSource);
+                Control.Adapter = adapter;
+            }
+            if (e.PropertyName == Dropdown.SelectedIndexProperty.PropertyName)
+            {
+                Control.SetSelection(view.SelectedIndex);
+            }
+            base.OnElementPropertyChanged(sender, e);
+        }
+
+        private void OnItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            var view = Element;
+            if (view != null)
+            {
+                view.SelectedIndex = e.Position;
+                view.OnItemSelected(e.Position);
+            }
+        }
+    }
+}
--- MobileApp/Mobile/Mobile/Mobile/Tracks.xaml.cs
+++ MobileApp/Mobile/Mobile/Mobile/Tracks.xaml.cs
@@ -1,20 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace Mobile
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Tracks : ContentPage
     {
+        List<string> Items1 = new List<string>();
+        List<string> Items2 = new List<string>();
+        bool IsItem1 = true;
+
         public Tracks()
         {
             InitializeComponent();
+
+            
+
+            Items1.Add("Hip-Hop");
+            Items1.Add("Pop");
+            Items1.Add("RNB");
+
+            Items2.Add("Price");
+            Items2.Add("Genre");
+            Items2.Add("Newest");
+            Items2.Add("Relevance");
+
+
+
+            dropdown.ItemsSource = Items1;
+            dropdown.SelectedIndex = 1;
+            dropdown.ItemSelected += OnDropdownSelected;
+
+            dropdown2.ItemsSource = Items2;
+            dropdown2.SelectedIndex = 1;
+            dropdown2.ItemSelected += OnDropdownSelected;
+        }
+
+        private void OnDropdownSelected(object sender, ItemSelectedEventArgs e)
+        {
+            label.Text = IsItem1 ? Items1[e.SelectedIndex] : Items2[e.SelectedIndex];
+        }
+
+        private void btn_Clicked(object sender, EventArgs e)
+        {
+            //dropdown.ItemsSource = IsItem1 ? Items2 : Items1;
+            //dropdown.SelectedIndex = IsItem1 ? 5 : 1;
+            //IsItem1 = !IsItem1;
         }
     }
 }
