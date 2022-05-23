23232d78cdd2fb4711824633b4cf764e55d5aa39
Konrad Jazdzyk <konradsmkkd@gmail.com>
2022-05-23 16:45:03 +0200

    ViewModels 1/2

---
+++ MobileApp/Mobile/Mobile/Mobile/Models/Music.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mobile.Models
+{
+    public class Music
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Url { get; set; }
+        public string CoverImage { get; set; } 
+        public bool IsRecent { get; set; }
+    }
+}
