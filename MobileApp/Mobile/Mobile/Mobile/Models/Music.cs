78e2b903f6cfbd4acef55b4678668ad151db72df
Konrad Jazdzyk <konradsmkkd@gmail.com>
2022-05-30 16:38:59 +0200

    add ViewModels

--- MobileApp/Mobile/Mobile/Mobile/Models/Music.cs
+++ MobileApp/Mobile/Mobile/Mobile/Models/Music.cs
@@ -7,7 +7,7 @@ namespace Mobile.Models
     public class Music
     {
         public string Title { get; set; }
-        public string Author { get; set; }
+        public string Artist { get; set; }
         public string Url { get; set; }
         public string CoverImage { get; set; } 
         public bool IsRecent { get; set; }
